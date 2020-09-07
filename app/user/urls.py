843cb66c42b47d595bb4cf6bf7ddc1c9af436b96
Jee Yi <jee.yi@carta.com>
2020-09-07 15:05:00 -0700

    Added endpoint for creating and authenticating users.

---
+++ app/user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from user import views
+
+
+app_name = "user"
+
+urlpatterns = [
+    path("create/", views.createUserView.as_view(), name="create"),
+    path("token/", views.createTokenView.as_view(), name="token"),
+]
