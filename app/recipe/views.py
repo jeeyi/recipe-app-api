4e55260953086127a695cc24858bb9eaf845b8d2
Jee Yi <jee.yi@carta.com>
2020-09-07 22:35:38 -0700

    Added list and create ingredient feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,13 +2,13 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
 
 class TagViewSet(
-    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
 ):
     """Manage tags in the database"""
 
@@ -21,6 +21,25 @@ class TagViewSet(
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
-    def perform_create(self, serializers):
+    def perform_create(self, serializer):
         """Create a new tag"""
-        serializers.save(user=self.request.user)
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
+    """Manage ingredients in the database"""
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
